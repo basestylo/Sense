7748f81376719aa86bff4b7dbee8238cceffde7f
David Martín García <davidmartingarcia0@gmail.com>
2018-11-16 04:16:06 +0100

    updating ws port, services and also fixing pod info for seeds

--- infra/deploy-mqtt.sh
+++ infra/deploy-mqtt.sh
@@ -14,14 +14,5 @@ gcloud auth configure-docker --quiet
 
 export PROJECT_ID="$(gcloud config get-value project -q)"
 
-# DB: Mosquitto custom with auth schema
-docker build ./mqtt -t sense_mqtt:latest
-
-docker tag sense_mqtt:latest eu.gcr.io/${PROJECT_ID}/sense_mqtt:$TAG
-docker tag sense_mqtt:latest eu.gcr.io/${PROJECT_ID}/sense_mqtt:latest
-
-docker push eu.gcr.io/${PROJECT_ID}/sense_mqtt:$TAG
-docker push eu.gcr.io/${PROJECT_ID}/sense_mqtt:latest
-
 # Deploy
 envsubst < kubernetes/mqtt-deployment.yaml | kubectl apply -f -
